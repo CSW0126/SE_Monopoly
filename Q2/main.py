15f04c6ca74a0bef2ec76ea4b348c2ea44d4c2a7
CSW0126 <shuiwing123@gmail.com>
2021-10-19 22:41:21 +0800

    Update main.py

--- Q2/main.py
+++ Q2/main.py
@@ -1,7 +1,30 @@
+
+blockList = []
+playerList = []
+
+
 def main():
     #Main program
     #import Menu.py
     #Call the method printMenu()
+    #if choice == 0:
+        #new Game
+
+        #ask how many player (2-6):
+        #generate players according of the input
+        #add all to playerList
+
+        #generate all Block object according to the property json on data.py
+        #add all to blockList
+
+    #if choice == 1:
+        #read save
+        #generate all player object according to the saved data
+        #generate all block object according to the property json on data.py
+        #set all game stats of gameBoard according to the saved data
+
+    
+    #GameBoard(playerList,blockList).run()
     pass
 
 if __name__ == "__main__":

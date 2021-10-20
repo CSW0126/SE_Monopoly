0bec6dfc73429c16d0a3612ef908ea4aedf6d27f
UalNamNuhc <33993002+Ualnomis@users.noreply.github.com>
2021-10-21 01:47:01 +0800

    class dia

--- Q2/Menu.py
+++ Q2/Menu.py
@@ -1,18 +1,18 @@
 
+class Menu:
+    def print_menu():
+        #print header
 
-def print_menu():
-    #print header
+        #print select options
 
-    #print select options
+        #if answer is 'New Game', return 0
 
-    #if answer is 'New Game', return 0
+        #if answer is 'Continue', return 1
 
-    #if answer is 'Continue', return 1
-
-    #if answer is 'Check Game Rule', print the game rule
-        #print the game rule
-        #show option back
-        #if back is selected, call print_menu() again
-    
-    #if answer is 'Exit', call exit()
-    pass
+        #if answer is 'Check Game Rule', print the game rule
+            #print the game rule
+            #show option back
+            #if back is selected, call print_menu() again
+        
+        #if answer is 'Exit', call exit()
+        pass
--- Q2/main.py
+++ Q2/main.py
@@ -1,31 +1,31 @@
+class Main:
+    block_list = []
+    player_list = []
 
-block_list = []
-player_list = []
 
+    def main():
+        #Main program
+        #import Menu.py
+        #Call the method print_menu()
+        #if choice == 0:
+            #new Game
 
-def main():
-    #Main program
-    #import Menu.py
-    #Call the method print_menu()
-    #if choice == 0:
-        #new Game
+            #ask how many player (2-6):
+            #generate players according of the input
+            #add all to player_list
 
-        #ask how many player (2-6):
-        #generate players according of the input
-        #add all to player_list
+            #generate all Block object according to the property json on data.py
+            #add all to block_list
 
-        #generate all Block object according to the property json on data.py
-        #add all to block_list
+        #if choice == 1:
+            #read save
+            #generate all player object according to the saved data
+            #generate all block object according to the property json on data.py
+            #set all game stats of game_board according to the saved data
 
-    #if choice == 1:
-        #read save
-        #generate all player object according to the saved data
-        #generate all block object according to the property json on data.py
-        #set all game stats of game_board according to the saved data
+        
+        #GameBoard(player_list,block_list).run()
+        pass
 
-    
-    #GameBoard(player_list,block_list).run()
-    pass
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()

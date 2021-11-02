28eb40a9015f0189c66a527b9e2465abd878ac6c
CSW0126 <shuiwing123@gmail.com>
2021-11-02 15:07:20 +0800

    test menu

--- Program/Menu.py
+++ Program/Menu.py
@@ -3,14 +3,22 @@ from whaaaaat import prompt
 from data import *
 
 class Menu:
+    def __init__(self):
+        self.is_test = False
+        self.select_data = []
 
     def print_menu(self):
         #print header
-        for line in game_logo:
-            print(line)
+        if not self.is_test:
+            for line in game_logo:
+                print(line)
 
         #print select options
-        answers = prompt(questions, style=style)
+        answers = {}
+        if not self.is_test:
+            answers = prompt(questions, style=style)
+        else:
+            answers['select'] = self.select_data[0]
 
         #if answer is 'New Game', return 0
         if answers['select'] == 'New Game': return 0
@@ -20,18 +28,30 @@ class Menu:
 
         #if answer is 'Check Game Rule', print the game rule
         elif answers['select'] == 'Check Game Rule':
-            #print the game rule
-            print("\n------------------------------------")
-            for line in rule:
-                print(line)
-            print("------------------------------------\n")
+            if not self.is_test:
+                #print the game rule
+                print("\n------------------------------------")
+                for line in rule:
+                    print(line)
+                print("------------------------------------\n")
 
-            #show option back
-            back = prompt(game_rule_back, style=style)
-            print(back)
+            back = {}
+            if not self.is_test:
+                #show option back
+                back = prompt(game_rule_back, style=style)
+            else:
+                back['choice'] = self.select_data[1]
+            #print(back)
             #if back is selected, call print_menu() again
             if back['choice'] == 'Back':
-                self.print_menu()
+                if not self.is_test:
+                    self.print_menu()
+                else:
+                    return 2
         
         #if answer is 'Exit', call exit()
-        else: exit()
+        else: 
+            if not self.is_test:
+                exit()
+            else:
+                return 3
--- Program/test_menu.py
+++ Program/test_menu.py
@@ -5,4 +5,29 @@ from Menu import *
 #py test_menu.py -v
 
 class TestMenu(unittest.TestCase):
-    pass
+    
+    def test_print_menu(self):
+        menu : Menu = Menu()
+        menu.is_test = True
+
+        #select new game
+        menu.select_data.append('New Game')
+        self.assertEqual(menu.print_menu(), 0)
+
+        #select continue
+        menu.select_data[0] = 'Continue'
+        self.assertEqual(menu.print_menu(), 1)
+
+        #select check game rule
+        menu.select_data[0] = 'Check Game Rule'
+        menu.select_data.append('Back')
+        self.assertEqual(menu.print_menu(), 2)
+
+        #select exit
+        menu.select_data[0] = 'Exit'
+        self.assertEqual(menu.print_menu(), 3)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
